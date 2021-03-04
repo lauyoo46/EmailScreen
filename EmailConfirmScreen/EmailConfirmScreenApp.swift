726ad38fa5d97978ca2b64a14b6f32011aad96f8
Laurentiu Ile <ilelaurentiu@yahoo.com>
2021-03-04 17:02:12 +0200

    Initial Commit

---
+++ EmailConfirmScreen/EmailConfirmScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailConfirmScreenApp.swift
+//  EmailConfirmScreen
+//
+//  Created by Laurentiu Ile on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmailConfirmScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
